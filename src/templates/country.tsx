b400be941182e31edd25fbb0d14c21241d4cacfc
ajay kumar <ajaykumar.yadav@dotsquares.com>
2023-06-29 13:01:08 +0530

    add country url

--- src/templates/country.tsx
+++ src/templates/country.tsx
@@ -231,7 +231,7 @@ const country: Template<TemplateRenderProps> = ({
       <li className=" storelocation-category">
         <a
           key={entity.slug}
-          href={detlslug +".html"}>
+          href={detlslug}>
           {entity.name} ({entity.dm_baseEntityCount})
         </a>
       </li>
